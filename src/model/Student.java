8b55f33408d54e0170befc2d361194ae5aec7c5d
Sambiti Chattopadhyay <sambitichattopadhyay@gmail.com>
2026-03-27 01:08:24 +0530

    Implement constructor for Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -6,4 +6,10 @@ public class Student {
     private String name;
     private int age;
     private String department;
+    public Student(int id, String name, int age, String department) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.department = department;
+}
 }
