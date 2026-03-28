fdd8ad10316268a9b10f4f6e372bcc7e6345ec63
Sambiti Chattopadhyay <sambitichattopadhyay@gmail.com>
2026-03-29 00:12:00 +0530

    Add all operations

--- src/service/StudentManager.java
+++ src/service/StudentManager.java
@@ -7,8 +7,29 @@ public class StudentManager implements StudentService {
 
     private ArrayList<Student> students = new ArrayList<>();
 
-    public void addStudent(Student s) {}
-    public void viewStudents() {}
-    public void searchStudent(int id) {}
-    public void deleteStudent(int id) {}
+    @Override
+public void addStudent(Student s) {
+    students.add(s);
+}
+
+@Override
+public void viewStudents() {
+    for(Student s : students){
+        s.display();
+    }
+}
+  @Override
+public void searchStudent(int id) {
+    for(Student s : students){
+        if(s.getId() == id){
+            s.display();
+            return;
+        }
+    }
+}
+
+@Override
+public void deleteStudent(int id) {
+    students.removeIf(s -> s.getId() == id);
+}
 }
